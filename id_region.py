e0ef47b310709a12deb5cebb6249775f08622a6a
jordenrabasco <jrabasc@ncsu.edu>
2023-12-31 15:15:16 -0500

    update id_region
    -main update

--- id_region.py
+++ id_region.py
@@ -15,47 +15,172 @@ from Bio import pairwise2
 from Bio.pairwise2 import format_alignment
 import json
 import random
+import math
 
 def seq_combos(curr_seq):
-    ref_dict = {'Y': ['C', 'T'], 'M': ['C', 'A']}
-    built_seq = ''
-    seq_dict = {}
-    seq_dict_key = 0
-    for base in curr_seq:
-        if base not in ref_dict.keys():
-            built_seq = built_seq + base
+    symbol_dict = {'A': ['A'], 'C': ['C'], 'G': ['G'], 'T': ['T'], 'R': ['G', 'A'], 'Y': ['C', 'T'], 'K': ['G', 'T'],
+                   'M': ['A', 'C'], 'S': ['G', 'C'], 'W': ['A', 'T'], 'B': ['G', 'T', 'C'], 'D': ['G', 'T', 'A'],
+                   'H': ['A', 'T', 'C'],
+                   'V': ['G', 'C', 'A'], 'N': ['G', 'C', 'A', 'T']}
+    var_dict = {}
+    dict_size = 1
+    temp_storage = []
+    indic_first = 0
+
+    for bp in curr_seq:
+        if indic_first == 0:
+            temp_storage = symbol_dict[bp]
+        dict_size = dict_size * len(symbol_dict[bp])
+        indic_first = indic_first + 1
+    for x in range(0, dict_size):
+        if len(temp_storage) > x:
+            var_dict[x] = temp_storage[x]
         else:
-            pot_bases = ref_dict[base]
-            for pot_base in pot_bases:
-                seq_dict_key = seq_dict_key + 1
+            var_dict[x] = ""
+    num_entries = 0
+    not_first = 0
+    for bp in curr_seq:
+        if not_first > 0:
+            temp_num_entries = num_entries
+            for keyness in range(0, len(var_dict.keys())):
+                if keyness <= temp_num_entries:
+                    if len(symbol_dict[bp]) > 1:
+                        indic = 0
+                        temp_seq = var_dict[keyness]
+                        for var_bp in symbol_dict[bp]:
+                            if indic != 0:
+                                num_entries = num_entries + 1
+                                var_dict[num_entries] = temp_seq + var_bp
+                            else:
+                                var_dict[keyness] = temp_seq + var_bp
+                            indic = indic + 1
+                    else:
+                        temp_seq = var_dict[keyness]
+                        temp_seq = temp_seq + symbol_dict[bp][0]
+                        var_dict[keyness] = temp_seq
+        not_first = not_first + 1
+
+    #converts dict of possible primer seqs to kmers where key is primer seq and value is list of kmers
+    kmer_dict = {}
+    for keyer in var_dict.keys():
+        primer_seq = var_dict[keyer]
+        kmer_list = generate_kmers(primer_seq, 8)
+        kmer_dict[primer_seq] = kmer_list
+    return kmer_dict
+
+def generate_kmers(sequence, k):
+    kmers = []
+    for i in range(len(sequence) - k + 1):
+        kmer = sequence[i:i + k]
+        kmers.append(kmer)
+    return kmers
+
+def find_subseq(seq, subseq):
+    start = seq.find(subseq)
+    end = start + len(subseq) - 1
+    return start, end
+
+
+def ham_dist(seq_one, seq_two):
+    if len(seq_one) != len(seq_two):
+        raise ValueError("Seqs dont have the same length")
+
+    dist = sum(bp_one != bp_two for bp_one, bp_two in zip(seq_one, seq_two))
+    return dist
+
+def sec_check(low_bound, up_bound, primer_seq, seq):
+    if((low_bound-(len(primer_seq)-1)) < 0):
+        lower_bound = 0
+    else:
+        lower_bound = low_bound - (len(primer_seq)-1)
+    if((up_bound+(len(primer_seq)-1)) > len(seq)-1):
+        upper_bound = len(seq)-1
+    else:
+        upper_bound = up_bound+(len(primer_seq)-1)
+    narrow_seq = seq[lower_bound:upper_bound]
+    kmer_list = generate_kmers(narrow_seq, len(primer_seq))
+    print("------------------")
+    print(low_bound)
+    print(up_bound)
+    print(lower_bound)
+    print(upper_bound)
+    print(kmer_list)
+    print(len(kmer_list[0]))
 
 
 
 
-    return seq_arr
+def find_dist(record_one, kmer_dict, primer_seq):
+    seq_id = record_one[0].id
+    seq = record_one[0].seq
+    length = len(seq)
+    score_arr = [0] * length
+    temp_arr=[]
+
+    # iterate through kmers for primer and if found add them to the score_arr
+    for keyer in kmer_dict.keys():
+        kmer_list = kmer_dict[keyer]
+        for kmer in kmer_list:
+            start, end = find_subseq(seq, kmer)
+            if start != -1:
+                for x in range(start, end + 1):
+                    temp = score_arr[x]
+                    score_arr[x] = temp + 1
+                    temp_arr.append(x)
+    max_index = score_arr.index(max(score_arr))
+    temp_index = max_index
+    temp_value = 1
+
+    # find distribution of kmers around max value (low_bound)
+    while (temp_value != 0 and temp_index != -1):
+        temp_value = score_arr[temp_index]
+        temp_index = temp_index - 1
+    low_bound = temp_index + 2
+    # find distribution of kmers around max value (upper_bound)
+    temp_index = max_index
+    temp_value = 1
+    while (temp_value != 0 and temp_index != len(score_arr)):
+        temp_value = score_arr[temp_index]
+        temp_index = temp_index + 1
+    up_bound = temp_index - 2
+
+    if((up_bound - low_bound+1) != len(primer_seq)):
+        test_val = int(math.sqrt(((up_bound - low_bound+1) - len(primer_seq))**2))
+        if test_val > int(len(primer_seq)/2):
+            sec_check(low_bound, up_bound, primer_seq, seq)
+        else:
+            return -99, -99, (up_bound - low_bound + 1), temp_arr
+    else:
+        return low_bound, up_bound, (up_bound - low_bound + 1), temp_arr
 
 
 
 
 
 fasta_dir = '/Users/jrabasc/Desktop/github/pseudo_pooling'
-primer_data_dict = {}
+primer_loc_dict = {}
+
+#generate kmer_lists for fwd and rev primers
+primer_530_f = 'GTGYCAGCMGCCGCGGTAA'
+primer_830_r = 'GGACTACNVGGGTWTCTAAT'
+fwd_kmer_dict = seq_combos(primer_530_f)
+rev_kmer_dict = seq_combos(primer_830_r)
+
+
+
+
 for filename in os.listdir(fasta_dir):
     if (".fasta" in filename):
         f = os.path.join(fasta_dir, filename)
         for (record_one) in zip(SeqIO.parse(f, "fasta")):
-            seq_id = record_one[0].id
-            #temp_arr = str(primer_id).split('_')
-            #primer_key = temp_arr[0]
+            prim_one_low_bound, prim_one_up_bound, prim_one_len, temp_guy = find_dist(record_one, fwd_kmer_dict, primer_530_f)
+            if(prim_one_low_bound == -99):
+                temp_seq = Seq(primer_530_f).reverse_complement()
+                fwd_kmer_dict = seq_combos(Seq(primer_530_f).reverse_complement())
+                prim_one_low_bound, prim_one_up_bound, prim_one_len, temp_guy = find_dist(record_one, fwd_kmer_dict, temp_seq)
 
-            primer_530_f = 'GTGYCAGCMGCCGCGGTAA'
-            primer_830_r = 'GGACTACNVGGGTWTCTAA'
-            primer_one_dict = seq_combos('GTGYCAGCMGCCGCGGTAA')
-
-            seq = record_one[0].seq
-            if seq_id not in primer_data_dict.keys():
-                primer_data_dict[seq_id] = [seq]
-            else:
-                primer_data_dict[seq_id].append(seq)
-
-
+            prim_two_low_bound, prim_two_up_bound, prim_two_len, two_temp_guy = find_dist(record_one, rev_kmer_dict, primer_830_r)
+            if(prim_two_low_bound == -99):
+                temp_seq = Seq(primer_830_r).reverse_complement()
+                rev_kmer_dict = seq_combos(Seq(primer_830_r).reverse_complement())
+                prim_two_low_bound, prim_two_up_bound, prim_two_len, two_temp_guy = find_dist(record_one, rev_kmer_dict, temp_seq)
